8010caa650c2885aea514ab741c14a8e519b0bba
Narmadha <narmadhachockalingam486@gmail.com>
2026-06-29 14:53:56 +0530

    non static

--- datatypes/datatypes.java
+++ datatypes/datatypes.java
@@ -1,3 +1,4 @@
+package datatypes;
 public class datatypes {
     public static void main(String[] args) {
         // Primitive Data Types
--- nonStaticContext/Student.java
+++ nonStaticContext/Student.java
@@ -4,8 +4,17 @@ class Student {
 
     int age = 20; // non-static variable
 
+    static int rollNo = 101; // static variable
+
+    static void show() { // static method
+        System.out.println(rollNo);
+        // System.out.println(age); // cannot access non-static variable in static method
+    }
+
     void display() { // non-static method
         System.out.println(age);
+        System.out.println(rollNo); // static variable can be accessed inside non-static method
+        show(); // static method can be called inside non-static method
     }
 
     public static void main(String[] args) {
